6c7394d98295220032d5052d29b2128fef923ff9
Neodyblue <sieramsn@gmail.com>
2014-12-06 01:23:30 +0000

    Added implementation of TStats

---
+++ src/tower/tower_stats.cc
@@ -0,0 +1,39 @@
+#include "tower/tower_stats.hh"
+
+TStats::TStats(int damages, int range, int radius, int cooldown)
+  : damages_(damages)
+  , range_(range)
+  , radius_(radius)
+  , cooldown_(cooldown)
+{}
+
+
+TStats& TStats::operator+=(TStats& other)
+{
+  return *this + other;
+}
+
+TStats& TStats::operator+(TStats& other)
+{
+  damages_ += other.damages_;
+  range_ += other.range_;
+  radius_ += other.radius_;
+  cooldown_ += other.cooldown_;
+
+  return *this;
+}
+
+TStats& TStats::operator-=(TStats& other)
+{
+  return *this - other;
+}
+
+TStats& TStats::operator-(TStats& other)
+{
+  damages_ -= other.damages_;
+  range_ -= other.range_;
+  radius_ -= other.radius_;
+  cooldown_ -= other.cooldown_;
+
+  return *this;
+}
