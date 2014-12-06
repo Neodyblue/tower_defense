3dfc6bce6f61f5807e1175d01779f7b01801e413
Neodyblue <sieramsn@gmail.com>
2014-12-06 01:59:20 +0000

    Added mob header & implem

---
+++ include/mob/mob.hh
@@ -0,0 +1,18 @@
+#ifndef MOB_HH
+# define MOB_HH
+
+# include "entity/entity.hh"
+# include "mob/mob_stats.hh"
+
+class Mob : public Entity
+{
+public:
+  Mob(Point pos, MStats base_stats); /* FIXME add sprite */
+  virtual ~Mob() override;
+  MStats& get_stats();
+
+private:
+  MStats stats_;
+};
+
+#endif /* end of include guard: MOB_HH */
---
+++ src/mob/mob.cc
@@ -0,0 +1,14 @@
+#include "mob/mob.hh"
+
+Mob::Mob(Point pos, MStats base_stats) /* FIXME add sprite */
+  : Entity(pos)
+  , stats_(base_stats)
+{}
+
+Mob::~Mob()
+{}
+
+MStats& Mob::get_stats()
+{
+  return stats_;
+}
