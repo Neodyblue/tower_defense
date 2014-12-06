0133b9c469a39ece71e98690bb078c6bddd833a6
Louis Groux <groux_l@epita.fr>
2014-12-06 16:28:41 +0100

    Adds action_types.hh files that contains the actions available to the user

---
+++ include/game/action_types.hh
@@ -0,0 +1,20 @@
+#ifndef ACTION_TYPES_HH_
+# define ACTION_TYPES_HH_
+
+/**
+ * \brief enum of the various actions
+ * available to our buttons. More may come.
+ */
+enum action
+{
+  PLAY,
+  PAUSE,
+  QUIT,
+  QUITMENU,
+  OPTION,
+  TWRSLCT,
+  TWRPLACE,
+  TWRDSTROY
+};
+
+#endif /* !ACTION_TYPES_HH_ */
