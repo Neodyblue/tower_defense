f8d0698e65a6a78b759b351183aebb4dd1420ece
Neodyblue <sieramsn@gmail.com>
2014-12-07 19:59:38 +0100

    Added different type of mob

--- include/mob/mob.hh
+++ include/mob/mob.hh
@@ -2,9 +2,11 @@
 # define MOB_HH
 
 # include <SFML/Graphics.hpp>
+# include <memory>
 
 # include "entity/entity.hh"
 # include "mob/mob_stats.hh"
+# include "mob/mob_type.hh"
 # include "tower/tower_attack.hh"
 # include "tower/tower.hh"
 
@@ -13,7 +15,7 @@ class Tower;
 class Mob : public Entity
 {
 public:
-  Mob(Point pos, MStats base_stats); /* FIXME add sprite */
+  Mob(Point pos, MStats base_stats, mob_type type); /* FIXME add sprite */
   virtual ~Mob() override;
 
   MStats get_stats();
@@ -24,10 +26,17 @@ public:
   Point get_dir();
   void draw(sf::RenderWindow& window);
 
+  static std::shared_ptr<Mob> get_goblin(Point p);
+  static std::shared_ptr<Mob> get_minion(Point p);
+  static std::shared_ptr<Mob> get_troll(Point p);
+  static std::shared_ptr<Mob> get_skeleton(Point p);
+  static std::shared_ptr<Mob> get_murloc(Point p);
+
 private:
   MStats stats_;
   MStats real_stats_;
   Point dir_;
+  mob_type type_;
 };
 
 #endif /* end of include guard: MOB_HH */
---
+++ include/mob/mob_type.hh
@@ -0,0 +1,13 @@
+#ifndef MOB_TYPE_HH
+# define MOB_TYPE_HH
+
+enum mob_type
+{
+  GOBLIN,
+  MINION,
+  TROLL,
+  SKELETON,
+  MURLOC
+};
+
+#endif /* end of include guard: MOB_TYPE_HH */
