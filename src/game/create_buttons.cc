eb9eb6f715020be7f73d206cfc4db327eadc9b5a
Louis Groux <groux_l@epita.fr>
2014-12-07 02:34:48 +0100

    Adds beginning of Game HUD

--- src/game/create_buttons.cc
+++ src/game/create_buttons.cc
@@ -43,25 +43,30 @@ std::vector<std::shared_ptr<Button>> add_game_buttons(sf::Font& f,
                                                       sf::Vector2u win_size)
 {
   std::vector<std::shared_ptr<Button>> sol;
-  sf::RectangleShape button(sf::Vector2f(win_size.x / 4, win_size.y / 4));
-  button.setFillColor(sf::Color::White);
+  sf::RectangleShape button(sf::Vector2f(win_size.x / 8, win_size.y / 16));
+  button.setFillColor(sf::Color(200, 0, 0, 200));
+  button.setOutlineThickness(-5);
+  button.setOutlineColor(sf::Color(200, 0, 0, 220));
 
-  sf::Text text("Play", f, 24);
+  sf::Text text("Menu", f, 24);
   text.setColor(sf::Color::Black);
 
-  Button but(PLAY, sf::Vector2f(win_size.x / 2, win_size.y / 3),
+  Button but(QUITMENU, sf::Vector2f(90 * win_size.x / 96, 22 * win_size.y / 32),
              button, text);
 
   std::shared_ptr<Button> tamere = std::make_shared<Button>(but);
   sol.push_back(tamere);
 
-  button = sf::RectangleShape(sf::Vector2f(win_size.x / 4, win_size.y / 4));
-  button.setFillColor(sf::Color::White);
+  button = sf::RectangleShape(sf::Vector2f(win_size.x / 8, win_size.y / 16));
+  button.setFillColor(sf::Color(0, 100, 0, 200));
+  button.setOutlineThickness(-5);
+  button.setOutlineColor(sf::Color(0, 100, 0, 220));
 
-  text = sf::Text("Play", f, 24);
+
+  text = sf::Text("Pause", f, 24);
   text.setColor(sf::Color::Black);
 
-  but = Button(QUIT, sf::Vector2f(win_size.x / 2, 2 * win_size.y / 3),
+  but = Button(PAUSE, sf::Vector2f(39 * win_size.x / 48, 22 * win_size.y / 32),
                button, text);
 
 
