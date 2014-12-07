86841159792785bb09230d1fcf1e8c6791ef55d4
Neodyblue <sieramsn@gmail.com>
2014-12-07 02:48:48 +0100

    Fixed duplicate member

--- include/play/play.hh
+++ include/play/play.hh
@@ -27,7 +27,6 @@ class Play
     std::vector<std::shared_ptr<Mob>> mobs_;
     std::vector<std::shared_ptr<Tower>> towers_;
     std::vector<Point> portals_;
-    std::vector<Tower> towers_;
     Point nexus_;
 
     int counter_;
